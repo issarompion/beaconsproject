273e68d6a15a384b10ef6151c85913052c72a4fc
issarompion <irompion@yahoo.fr>
2020-05-08 11:10:30 +0200

    update

--- back/microservices/lib/utils/kafka.ts
+++ back/microservices/lib/utils/kafka.ts
@@ -1,7 +1,5 @@
-import 'module-alias/register';
-import {Producer, Offset, KafkaClient} from 'kafka-node'
+import {Producer} from 'kafka-node'
 import { BeaconMessage, ClientMessage, AuthMessage, ContentMessage } from "../IMessage";
-import {ENV} from 'lib';
 
 export const sendKafkaMessage = ( prod: Producer, topicVal: string, msg: BeaconMessage | ClientMessage | AuthMessage | ContentMessage  ) => {
        prod.send([{ topic: topicVal, messages: JSON.stringify(msg) }], () => {
