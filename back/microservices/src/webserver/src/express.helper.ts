48a063b675885160241d2118a2587e25298e220c
issarompion <irompion@yahoo.fr>
2020-05-09 02:03:52 +0200

    add Authorization Header

--- back/microservices/src/webserver/src/express.helper.ts
+++ back/microservices/src/webserver/src/express.helper.ts
@@ -4,7 +4,7 @@ export const app = express();
 app.use(function (req: any, res: any, next: any) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
