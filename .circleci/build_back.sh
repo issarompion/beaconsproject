70f7b6f3e8eb7e0420e55510ca14e932f7299ba8
issarompion <irompion@yahoo.fr>
2020-05-17 00:17:55 +0200

    Procfile

---
+++ .circleci/build_back.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker build -f ./back/images/auth.Dockerfile --tag $DOCKERHUB_USERNAME/auth:$PROJECT_VERSION --build-arg GIT=$GIT .
+docker build -f ./back/images/beacon/beacon.Dockerfile --tag $DOCKERHUB_USERNAME/beacon:$PROJECT_VERSION --build-arg GIT=$GIT .
+docker build -f ./back/images/client.Dockerfile --tag $DOCKERHUB_USERNAME/client:$PROJECT_VERSION --build-arg GIT=$GIT .
+docker build -f ./back/images/content.Dockerfile --tag $DOCKERHUB_USERNAME/content:$PROJECT_VERSION --build-arg GIT=$GIT .
+docker build -f ./back/images/logger.Dockerfile --tag $DOCKERHUB_USERNAME/logger:$PROJECT_VERSION --build-arg GIT=$GIT .
+docker build -f ./back/images/webserver.Dockerfile --tag $DOCKERHUB_USERNAME/webserver:$PROJECT_VERSION --build-arg GIT=$GIT --build-arg API_PORT=$API_PORT .
+
+# how to use
+# source .env
+# sh .circleci/build_back.sh $DOCKERHUB_USERNAME $PROJECT_VERSION $GIT $API_PORT
+# docker rmi $DOCKERHUB_USERNAME/client:$PROJECT_VERSION $DOCKERHUB_USERNAME/beacon:$PROJECT_VERSION $DOCKERHUB_USERNAME/content:$PROJECT_VERSION $DOCKERHUB_USERNAME/logger:$PROJECT_VERSION $DOCKERHUB_USERNAME/webserver:$PROJECT_VERSION 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: npm run build:app && npm run prod:app
